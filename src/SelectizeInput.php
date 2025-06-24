700dcba6d9452c92ad222334db80e99762576468
Bender CarDoo <Bender_1011@list.ru>
2025-06-24 23:39:38 +0500

    Create SelectizeInput.php

---
+++ src/SelectizeInput.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace bender\selectize;
+
+use yii\helpers\Html;
+use yii\widgets\InputWidget;
+use yii\helpers\Json;
+
+class SelectizeInput extends InputWidget
+{
+    public $items = [];
+    public $clientOptions = [];
+
+    public function run()
+    {
+        SelectizeAsset::register($this->getView());
+
+        $id = $this->options['id'] ?? $this->getId();
+        $options = Json::encode($this->clientOptions);
+
+        echo Html::dropDownList(
+            $this->name,
+            $this->value,
+            $this->items,
+            array_merge($this->options, ['id' => $id])
+        );
+
+        $this->getView()->registerJs("$('#{$id}').selectize({$options});");
+    }
+}
