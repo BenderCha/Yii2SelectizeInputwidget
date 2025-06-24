7b8851996d081db52d62250c6334e6ab0ec6c97a
Bender CarDoo <Bender_1011@list.ru>
2025-06-24 23:40:23 +0500

    Create SelectizeAsset.php

---
+++ assets/SelectizeAsset.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace bender\selectize;
+
+use yii\web\AssetBundle;
+
+class SelectizeAsset extends AssetBundle
+{
+    public $css = [
+        'https://cdn.jsdelivr.net/npm/selectize@0.13.6/dist/css/selectize.default.css',
+    ];
+
+    public $js = [
+        'https://cdn.jsdelivr.net/npm/selectize@0.13.6/dist/js/standalone/selectize.min.js',
+    ];
+
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+}
